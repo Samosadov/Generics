a7eaba26a26c021fc162a00e062298fef1285dce
Samosadov <for-spam2@yandex.ru>
2023-01-11 23:09:47 +0300

    lists and map

--- src/ru_synergy/Main.java
+++ src/ru_synergy/Main.java
@@ -1,6 +1,7 @@
 package ru_synergy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -23,6 +24,27 @@ public class Main {
         int sum = 0;
         for (int num : nums) sum += num;
         System.out.println(sum);
+
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(92, "$ 50.00");
+        map.put(95, "$ 665.99");
+
+        ArrayList<String> listHello = new ArrayList<>();
+        listHello.add("hi");
+        listHello.add("hello");
+
+        ArrayList<String> listBye = new ArrayList<>();
+        listBye.add("bye");
+        listBye.add("hasta la vista");
+
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        list.add(listHello);
+        list.add(listBye);
+
+        for (ArrayList<String> arrList : list) {
+            for (String s : arrList) System.out.println(s);
+        }
+
 
     }
 }
