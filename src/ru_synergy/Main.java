7e68960ec776062b0771d4bfceebe220af33a5d9
Samosadov <for-spam2@yandex.ru>
2023-01-11 22:44:05 +0300

    cast failure

--- src/ru_synergy/Main.java
+++ src/ru_synergy/Main.java
@@ -1,5 +1,6 @@
 package ru_synergy;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,12 +11,18 @@ public class Main {
         Scanner scanner = null;
         if (o instanceof Scanner) scanner = (Scanner) o;
         if (scanner != null) scanner.nextInt();*/
-        Object[] objects = {128, "hi, suckers", 2.7}
+       /* Object[] objects = {128, "hi, suckers", 2.7}
         for(Object o : objects) {
             if (o instanceof String) {
                 String s = (String) o;
                 System.out.println(s);
-            }
+            }*/
+        ArrayList nums = new ArrayList();
+        for (int i = 0; i < 10; i++)
+            nums.add(i * 2);
+        int sum = 0;
+        for (Object num : nums) sum += (Integer) num;
+        System.out.println(sum);
         }
     }
 }
