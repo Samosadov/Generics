2ca967af986f306d8ffaaf43a40b3c68d9234c5f
Samosadov <for-spam2@yandex.ru>
2023-01-11 22:39:14 +0300

    instanceof

--- src/ru_synergy/Main.java
+++ src/ru_synergy/Main.java
@@ -6,9 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Object o = new Scanner(System.in);
+/*        Object o = new Scanner(System.in);
         Scanner scanner = null;
         if (o instanceof Scanner) scanner = (Scanner) o;
-        if (scanner != null) scanner.nextInt();
+        if (scanner != null) scanner.nextInt();*/
+        Object[] objects = {128, "hi, suckers", 2.7}
+        for(Object o : objects) {
+            if (o instanceof String) {
+                String s = (String) o;
+                System.out.println(s);
+            }
+        }
     }
 }
