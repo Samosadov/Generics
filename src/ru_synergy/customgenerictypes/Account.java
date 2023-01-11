d88f9b4d40a0ff604d2a898eecdb1b2f53f2108a
Samosadov <for-spam2@yandex.ru>
2023-01-11 23:22:51 +0300

    some lazha

---
+++ src/ru_synergy/customgenerictypes/Account.java
@@ -0,0 +1,28 @@
+package ru_synergy.customgenerictypes;
+
+public class Account {
+    private Object id;
+    private int sum;
+
+    public Account(Object id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public Object getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+}
--- src/ru_synergy/customgenerictypes/Main.java
+++ src/ru_synergy/customgenerictypes/Main.java
@@ -2,6 +2,12 @@ package ru_synergy.customgenerictypes;
 
 public class Main {
     public static void main(String[] args) {
+        Account account = new Account(12, 50);
+        System.out.println(account.getId());
 
+        Account account1 = new Account("1", 45);
+        System.out.println(account1.getId());
+
+        int a = (Integer) account.getId() + 1;
     }
 }
