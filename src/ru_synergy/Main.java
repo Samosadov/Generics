c9d839ea2ffaf4102374d9a092affdaf8a926b8e
Samosadov <for-spam2@yandex.ru>
2023-01-11 22:34:48 +0300

    added gitignore

--- src/ru_synergy/Main.java
+++ src/ru_synergy/Main.java
@@ -1,8 +1,14 @@
 package ru_synergy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Object o = new Scanner(System.in);
+        Scanner scanner = null;
+        if (o instanceof Scanner) scanner = (Scanner) o;
+        if (scanner != null) scanner.nextInt();
     }
 }
